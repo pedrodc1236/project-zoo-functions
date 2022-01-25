33ac6de96dac6310fffdfc8840f1f2be4a04d486
Pedro Tavares <pedrodc1236@gmail.com>
2022-01-25 17:15:08 -0300

    requisito 1 completo

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,18 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-  
+function getSpeciesByIds(...ids) {
+  return ids.map((id) =>
+    species.find((specie) => specie.id === id));
 }
+
+console.log(getSpeciesByIds());
+
+console.log(
+  getSpeciesByIds(
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    'baa6e93a-f295-44e7-8f70-2bcdc6f6948d',
+  ),
+);
 
 module.exports = getSpeciesByIds;
