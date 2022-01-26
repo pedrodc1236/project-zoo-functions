402d51ce9394e5626844aaff50c9de3502ec5a78
Pedro Tavares <pedrodc1236@gmail.com>
2022-01-26 14:35:15 -0300

    requisito 4 completo

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,25 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employee) =>
+    employee.managers.some((manager) => manager === id));
 }
 
+// console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) {
+    const arr = employees
+      .filter((employee) => employee.managers
+        .find((manager) => manager === managerId));
+    return arr.map((obj) => `${obj.firstName} ${obj.lastName}`);
+  }
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 }
+
+// console.log((getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c099')));
 
 module.exports = { isManager, getRelatedEmployees };
