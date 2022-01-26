09b9a419f5fda009b62c9a9f3f381dd53aebb35e
Pedro Tavares <pedrodc1236@gmail.com>
2022-01-26 19:11:39 -0300

    requisito 5 completo

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const newObj = {};
+    species.forEach((specie) => {
+      newObj[specie.name] = specie.residents.length;
+    });
+    return newObj;
+  }
+  const { specie, sex } = animal;
+  if (Object.keys(animal).length === 1) {
+    return species.filter((element) => element.name === specie)[0]
+      .residents.length;
+  }
+  if (Object.keys(animal).length === 2) {
+    return species
+      .filter((element) => element.name === specie)[0]
+      .residents.filter((resident) => resident.sex === sex).length;
+  }
 }
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
